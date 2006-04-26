b45c0f63068d0a0b2aa78ce7df6981b1516e2c71
Dave Airlie <airlied@linux.ie>
2006-04-26 08:38:47 +0000

    Make the ATI driver use the PCI BAR size to determine the MMIO register     aperture size. Otherwise it tries to overmap things if they are up near     the end of memory. (cherry picked from 85e2ee6de6fd347aad66785ec7119f92013cd994 commit)

--- src/radeon_dri.c
+++ src/radeon_dri.c
@@ -1023,7 +1023,7 @@ static Bool RADEONDRIPciInit(RADEONInfoPtr info, ScreenPtr pScreen)
 static Bool RADEONDRIMapInit(RADEONInfoPtr info, ScreenPtr pScreen)
 {
 				/* Map registers */
-    info->registerSize = RADEON_MMIOSIZE;
+    info->registerSize = info->MMIOSize;
     if (drmAddMap(info->drmFD, info->MMIOAddr, info->registerSize,
 		  DRM_REGISTERS, DRM_READ_ONLY, &info->registerHandle) < 0) {
 	return FALSE;
