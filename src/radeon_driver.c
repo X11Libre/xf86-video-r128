6ff77d071ce7b30930f696814b4dfc0abe207036
Michel Dänzer <michel@tungstengraphics.com>
2006-07-02 17:03:23 +0200

    Bug #6939: Disable RENDER acceleration by default on RN50.
    Based on patch from Weixing Zhang, but still allows enabling it with Option
    "RenderAccel".
    (cherry picked from 11bedb7c845fc6074383890d5525315aebe05f2c commit)

--- src/radeon_driver.c
+++ src/radeon_driver.c
@@ -3012,8 +3012,9 @@ static Bool RADEONPreInitChipType(ScrnInfoPtr pScrn)
 		   "Option ShowCache enabled\n");
 
 #ifdef RENDER
-    info->RenderAccel = xf86ReturnOptValBool (info->Options,
-					      OPTION_RENDER_ACCEL, TRUE);
+    info->RenderAccel = xf86ReturnOptValBool(info->Options, OPTION_RENDER_ACCEL,
+					     info->Chipset != PCI_CHIP_RN50_515E &&
+					     info->Chipset != PCI_CHIP_RN50_5969);
 #endif
 
     return TRUE;
