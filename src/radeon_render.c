84e574986f61c4f250d6ecb938bab20847618906
Dave Airlie <airlied@starflyer-ubuntu.(none)>
2006-09-12 03:33:35 +1000

    radeon: fallback render repeat for XAA, this fixes corruptions seen on ubuntu
    This isn't the correct fix, we should just fix the repeat case, but for a
    stable release this is much better idea

--- src/radeon_render.c
+++ src/radeon_render.c
@@ -389,6 +389,10 @@ static Bool FUNC_NAME(R100SetupTexture)(
 #endif
     ACCEL_PREAMBLE();
 
+    /* render repeat is broken - fix in stable tree by falling back */
+    if (flags & XAA_RENDER_REPEAT)
+	return FALSE;
+
     if ((width > 2048) || (height > 2048))
 	return FALSE;
 
@@ -720,6 +724,10 @@ static Bool FUNC_NAME(R200SetupTexture)(
     CARD8 *tmp_dst;
 #endif
     ACCEL_PREAMBLE();
+
+    /* render repeat is broken - fix in stable tree by falling back */
+    if (flags & XAA_RENDER_REPEAT)
+	return FALSE;
 
     if ((width > 2048) || (height > 2048))
 	return FALSE;
