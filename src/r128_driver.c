178e6f509bb8b351cfa01425ada7331781843954
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-16 11:51:08 +1000

    Update to xextproto 7.1 support.
    DPMS header was split into dpms.h (client) and dpmsconst.h (server). Drivers
    need to include dpmsconst.h if xextproto 7.1 is available.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/r128_driver.c
+++ src/r128_driver.c
@@ -101,8 +101,13 @@
 #include "dixstruct.h"
 
 				/* DPMS support. */
+#ifdef HAVE_XEXTPROTO_71
+#include <X11/extensions/dpmsconst.h>
+#else
 #define DPMS_SERVER
 #include <X11/extensions/dpms.h>
+#endif
+
 
 #ifndef MAX
 #define MAX(a,b) ((a)>(b)?(a):(b))
