e9cf08f26c897b3f2de40eba7c07d2e1e47a14c1
Alan Coopersmith <alan.coopersmith@oracle.com>
2022-10-17 11:13:00 -0700

    R128RAMRec: constify name pointer
    Clears 4 gcc warnings:
    r128_driver.c:178:36: warning: initialization discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
         { 4, 4, 3, 3, 1, 3, 1, 16, 12, "128-bit SDR SGRAM 1:1" },
                                        ^~~~~~~~~~~~~~~~~~~~~~~
    r128_driver.c:179:36: warning: initialization discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
         { 4, 8, 3, 3, 1, 3, 1, 17, 13, "64-bit SDR SGRAM 1:1" },
                                        ^~~~~~~~~~~~~~~~~~~~~~
    r128_driver.c:180:36: warning: initialization discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
         { 4, 4, 1, 2, 1, 2, 1, 16, 12, "64-bit SDR SGRAM 2:1" },
                                        ^~~~~~~~~~~~~~~~~~~~~~
    r128_driver.c:181:36: warning: initialization discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
         { 4, 4, 3, 3, 2, 3, 1, 16, 12, "64-bit DDR SGRAM" },
                                        ^~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/r128.h
+++ src/r128.h
@@ -154,7 +154,7 @@ typedef struct {        /* All values in XCLKS    */
     int  Tr2w;          /* Read to Write Delay    */
     int  Rloop;         /* Loop Latency           */
     int  Rloop_fudge;   /* Add to ML to get Rloop */
-    char *name;
+    const char *name;
 } R128RAMRec, *R128RAMPtr;
 
 typedef struct {
